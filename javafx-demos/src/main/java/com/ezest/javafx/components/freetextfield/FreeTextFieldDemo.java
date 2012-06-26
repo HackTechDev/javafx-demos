258fdeafb2a1dcd9c2dcd28542b7d989be788725
saipradeep.dandem <saipradeep.dandem@gmail.com>
2012-06-26 13:42:05 +0000

    Free text field comitt

--- javafx-demos/src/main/java/com/ezest/javafx/components/freetextfield/FreeTextFieldDemo.java
+++ javafx-demos/src/main/java/com/ezest/javafx/components/freetextfield/FreeTextFieldDemo.java
@@ -1,15 +1,28 @@
 package com.ezest.javafx.components.freetextfield;
 
-import com.javafx.experiments.scenicview.ScenicView;
-
 import javafx.application.Application;
-import javafx.scene.GroupBuilder;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPaneBuilder;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.StackPaneBuilder;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import com.javafx.experiments.scenicview.ScenicView;
 
 public class FreeTextFieldDemo extends Application {
 
@@ -26,29 +39,65 @@ public class FreeTextFieldDemo extends Application {
 		configureScene();
 		configureStage();
 		
-		FreeTextField text = new FreeTextField();
-		StackPane sp = StackPaneBuilder.create().minHeight(50).minWidth(300).children(text).build();
+		ScrollFreeTextArea text1 = new ScrollFreeTextArea();
+		ScrollFreeTextArea text2 = new ScrollFreeTextArea();
+		ScrollFreeTextArea text3 = new ScrollFreeTextArea();
 		
-		root.getChildren().add(GroupBuilder.create().children(sp).build());
+		/*FreeTextField text1 = new FreeTextField();
+		FreeTextField text2 = new FreeTextField();
+		FreeTextField text3 = new FreeTextField();
+		*/
+		VBox vb = VBoxBuilder.create().spacing(15).build();
+		
+		vb.getChildren().addAll(new Label("Description 1 : "), text1);
+		vb.getChildren().addAll(new Label("Description 2 : "), text2);
+		vb.getChildren().addAll(new Label("Description 3 : "), text3);
+		
+		StackPane sp = StackPaneBuilder.create()
+								.padding(new Insets(15))
+								.children(vb).build();
+		
+		
+		root.getChildren().add(ScrollPaneBuilder.create().fitToHeight(true).fitToWidth(true).content(sp).build());
 	}
 
 	private void configureStage(){
 		stage.setTitle(this.getClass().getSimpleName());
 		stage.setX(0);
 	    stage.setY(0);
-	    stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
+	    stage.setWidth(600);
 	    stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
 	    stage.setScene(this.scene);
 	    stage.show();
-	    ScenicView.show(this.scene);
 	}
 	
 	private void configureScene(){
 		root = new StackPane();
-		root.autosize();
-		this.scene = new Scene(root, Color.LEMONCHIFFON);
+		root.setPadding(new Insets(15));
+		BorderPane bp = new BorderPane();
+		bp.setBottom(getBottom());
+		bp.setCenter(root);
+		bp.autosize();
+		this.scene = new Scene(bp,600d,600d, Color.LEMONCHIFFON);
 		//scene.getStylesheets().add("styles/template.css");
 	}
-
+	
+	private Node getBottom() {
+		StackPane sp = new StackPane();
+		sp.setMinHeight(25);
+		sp.setAlignment(Pos.TOP_RIGHT);
+		ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/images/mglass.gif")));
+		image.setCursor(Cursor.HAND);
+		image.setTranslateX(-5);
+		image.setTranslateY(3);
+		image.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent paramT) {
+				ScenicView.show(scene);
+			}
+		});
+		sp.getChildren().addAll(new Separator(),image);
+		return sp;
+	}
 }
 
