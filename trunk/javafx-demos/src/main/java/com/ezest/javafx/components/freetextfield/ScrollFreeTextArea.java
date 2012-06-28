1821fbf81cc4d639d0dc0d0752307ffeaaf19853
saipradeep.dandem@gmail.com <saipradeep.dandem@gmail.com@4c216fad-b91a-775a-f804-78c4b1859aa8>
2012-06-28 05:37:52 +0000

    Comitt

--- trunk/javafx-demos/src/main/java/com/ezest/javafx/components/freetextfield/ScrollFreeTextArea.java
+++ trunk/javafx-demos/src/main/java/com/ezest/javafx/components/freetextfield/ScrollFreeTextArea.java
@@ -1,91 +1,55 @@
 package com.ezest.javafx.components.freetextfield;
 
-import javafx.beans.binding.StringBinding;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.GroupBuilder;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.StackPaneBuilder;
 
-/**
- * TextArea component which does not have a scroll enabled.
- * @author Sai.Dandem
- */
 public class ScrollFreeTextArea extends StackPane{
 
 	private Label label;
+	private StackPane lblContainer ;
 	private TextArea textArea;
-	private Character enterChar = new Character((char)10);
-	private Region content;
-	private SimpleDoubleProperty contentHeight = new SimpleDoubleProperty();
 	
-	
+	private Character NEW_LINE_CHAR = new Character((char)10);
 	private final double NEW_LINE_HEIGHT = 18D;
 	private final double TOP_PADDING = 3D;
 	private final double BOTTOM_PADDING = 6D;
-	private StackPane lblContainer;
-	private StackPane lblContainer2;
 	
 	public ScrollFreeTextArea(){
 		super();
 		configure();
 	}
 	
+	public ScrollFreeTextArea(String text){
+		super();
+		configure();
+		textArea.setText(text);
+	}
+	
 	private void configure(){
 		setAlignment(Pos.TOP_LEFT);
 		
-		this.textArea =new TextArea(){
-			protected void layoutChildren() {
-				super.layoutChildren();
-				if(content==null){
-					content = (Region)lookup(".content");
-					contentHeight.bind(content.heightProperty());
-					content.heightProperty().addListener(new ChangeListener<Number>() {
-						@Override
-						public void changed(ObservableValue<? extends Number> paramObservableValue,	Number paramT1, Number paramT2) {
-							//System.out.println("Content View Height :"+paramT2.doubleValue());
-						}
-					});
-				}
-			};
-		};
+		this.textArea =new TextArea();
 		this.textArea.setWrapText(true);
+		this.textArea.getStyleClass().add("scroll-free-text-area");
+		
 		
 		this.label =new Label();
 		this.label.setWrapText(true);
 		this.label.prefWidthProperty().bind(this.textArea.widthProperty());
-		//this.label.textProperty().bind(this.textArea.textProperty());
-		label.textProperty().bind(new StringBinding() {
-			{
-				bind(textArea.textProperty());
-			}
-			@Override
-			protected String computeValue() {
-				if(textArea.getText()!=null && textArea.getText().length()>0){
-					if(!((Character)textArea.getText().charAt(textArea.getText().length()-1)).equals(enterChar)){ 
-						System.out.println("enter added..");
-						return textArea.getText()+enterChar;
-					}
-				}
-				return textArea.getText();
-			}
-		});
-		
-		lblContainer = StackPaneBuilder.create()
+		this.label.textProperty().bind(this.textArea.textProperty());
+		 
+		this.lblContainer = StackPaneBuilder.create()
 										  .alignment(Pos.TOP_LEFT)
 										  .padding(new Insets(4,7,7,7))
 										  .children(label)
 										  .build();
-		lblContainer2 = StackPaneBuilder.create()
-				  .alignment(Pos.TOP_LEFT)
-				  .children(lblContainer)
-				  .build();
-
 		// Binding the container width to the TextArea width.
 		lblContainer.maxWidthProperty().bind(textArea.widthProperty());
 		
@@ -103,28 +67,19 @@ public class ScrollFreeTextArea extends StackPane{
 			}
 		});
 		
-		getChildren().addAll(lblContainer2,textArea);
+		getChildren().addAll(GroupBuilder.create().children(lblContainer).build(),textArea);
 	}
 	
 	private void layoutForNewLine(String text){
 		if(text!=null && text.length()>0 && 
-					((Character)text.charAt(text.length()-1)).equals(enterChar)){ 
+					((Character)text.charAt(text.length()-1)).equals(NEW_LINE_CHAR)){ 
 			textArea.setPrefHeight(label.getHeight() + NEW_LINE_HEIGHT + TOP_PADDING + BOTTOM_PADDING);
 			textArea.setMinHeight(label.getHeight() + NEW_LINE_HEIGHT + TOP_PADDING + BOTTOM_PADDING);
-			//setMinHeight(label.getHeight() + NEW_LINE_HEIGHT + TOP_PADDING + BOTTOM_PADDING);
-			lblContainer2.setPrefHeight(label.getHeight() + NEW_LINE_HEIGHT + TOP_PADDING + BOTTOM_PADDING);
-			lblContainer2.setMinHeight(label.getHeight() + NEW_LINE_HEIGHT + TOP_PADDING + BOTTOM_PADDING);
-			
 		}else{
 			textArea.setPrefHeight(label.getHeight() + TOP_PADDING + BOTTOM_PADDING);
 			textArea.setMinHeight(label.getHeight() + TOP_PADDING + BOTTOM_PADDING);
-			lblContainer2.setPrefHeight(label.getHeight() + TOP_PADDING + BOTTOM_PADDING);
-			lblContainer2.setMinHeight(label.getHeight() + TOP_PADDING + BOTTOM_PADDING);
-			/*textArea.setPrefHeight(contentHeight.get() + TOP_PADDING + BOTTOM_PADDING);
-			textArea.setMinHeight(contentHeight.get() + TOP_PADDING + BOTTOM_PADDING);
-			*///setMinHeight(label.getHeight() + TOP_PADDING + BOTTOM_PADDING);
-			
-			
 		}
 	}
+	
 }
+
