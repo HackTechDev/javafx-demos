d945be3b2a257b20c8e479c9a7850411af08f5c1
saipradeep.dandem <saipradeep.dandem@gmail.com>
2012-07-17 15:16:29 +0000

    Css Changes

--- javafx-demos/src/main/java/com/ezest/javafx/demogallery/webview/JavaFX_To_JavaScript_CommunicationDemo.java
+++ javafx-demos/src/main/java/com/ezest/javafx/demogallery/webview/JavaFX_To_JavaScript_CommunicationDemo.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
