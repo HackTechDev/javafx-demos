a061ba9adef74592f2eae5e213c263034946d551
saipradeep.dandem <saipradeep.dandem@gmail.com>
2012-08-29 13:33:04 +0000

    Demos of Javafx 2.2

---
+++ javafx-demos/src/main/java/com/ezest/javafx/demogallery/javafx2_2/SnapShotDemo.java
@@ -0,0 +1,106 @@
+package com.ezest.javafx.demogallery.javafx2_2;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import com.javafx.experiments.scenicview.ScenicView;
+
+public class SnapShotDemo extends Application {
+
+	Stage stage;
+	Scene scene;
+	StackPane root;
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		this.stage = stage;
+		configureScene();
+		configureStage();
+		
+		VBox vb = new VBox();
+		
+		Button btn = new Button("ScreenShot");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				final WritableImage RASTER = new WritableImage(300, 300);
+				scene.snapshot(RASTER);
+				
+				 ImageView img = new ImageView(RASTER);
+				 StackPane sp = new StackPane();
+				 sp.getChildren().add(img);
+				Stage stg = new Stage();
+				stg.setWidth(340);
+				stg.setHeight(370);
+				stg.setScene(new Scene(sp));
+				stg.show();
+				
+			}
+		});
+		vb.getChildren().addAll(new TextField(), btn);
+		root.getChildren().add(vb);
+		// Logic starts
+	}
+
+	private void configureStage(){
+		stage.setTitle(this.getClass().getSimpleName());
+		stage.setX(0);
+	    stage.setY(0);
+	    stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
+	    stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
+	    stage.setScene(this.scene);
+	    stage.show();
+	}
+	
+	private void configureScene(){
+		root = new StackPane();
+		BorderPane bp = new BorderPane();
+		bp.setBottom(getBottom());
+		bp.setCenter(root);
+		bp.autosize();
+		this.scene = new Scene(bp, Color.LINEN);
+		scene.getStylesheets().add("styles/template.css");
+	}
+
+	private Node getBottom() {
+		StackPane sp = new StackPane();
+		sp.setMinHeight(25);
+		sp.setAlignment(Pos.TOP_RIGHT);
+		ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/images/mglass.gif")));
+		image.setCursor(Cursor.HAND);
+		image.setTranslateX(-5);
+		image.setTranslateY(3);
+		image.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent paramT) {
+				ScenicView.show(scene);
+			}
+		});
+		sp.getChildren().addAll(new Separator(),image);
+		return sp;
+	}
+
+}
+
+
