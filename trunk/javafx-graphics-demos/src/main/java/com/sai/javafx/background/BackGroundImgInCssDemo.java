7a13d8a65b0f5dbb51ee972346a28d2f57a06a36
saipradeep.dandem@gmail.com <saipradeep.dandem@gmail.com@4c216fad-b91a-775a-f804-78c4b1859aa8>
2012-12-24 12:46:24 +0000

    Added background css image implementation.

--- trunk/javafx-graphics-demos/src/main/java/com/sai/javafx/background/BackGroundImgInCssDemo.java
+++ trunk/javafx-graphics-demos/src/main/java/com/sai/javafx/background/BackGroundImgInCssDemo.java
@@ -1,8 +1,11 @@
 package com.sai.javafx.background;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPaneBuilder;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.StackPaneBuilder;
+import javafx.scene.layout.VBox;
 
 import com.sai.javafx.common.ExtendedApplicationX;
 
@@ -11,12 +14,18 @@ public class BackGroundImgInCssDemo extends ExtendedApplicationX{
 	@Override
 	protected void setup() {
 		StackPane body = StackPaneBuilder.create().styleClass("backgroundBody").build();
+		ScrollPane sp = ScrollPaneBuilder.create().styleClass("backgroundBody").fitToHeight(true).fitToWidth(true).build();
 		StackPane header = StackPaneBuilder.create().styleClass("backgroundHeader").prefHeight(36).build();
 		StackPane footer = StackPaneBuilder.create().styleClass("backgroundFooter").prefHeight(31).build();
 		BorderPane bp = new BorderPane();
-		bp.setCenter(body);
+		bp.setCenter(sp);
 		bp.setTop(header);
 		bp.setBottom(footer);
+		VBox vb = new VBox();
+		for (int i = 0; i < 10; i++) {
+			vb.getChildren().add(StackPaneBuilder.create().minHeight(181).styleClass("backgroundShelf").build());
+		}
+		sp.setContent(vb);
 		root.getChildren().add(bp);
 		scene.getStylesheets().add(this.getClass().getResource("/styles/template.css").toString());
 	}
